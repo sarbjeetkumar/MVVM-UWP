9ca7fbd9833a99b8aadf4aae095856940f1a1c39
sarab singh <sarab singh>
2016-09-21 23:02:21 +0100

    Helper Class

---
+++ LAb1/LAb1/LAb1.Windows/Models/Organization.cs
@@ -0,0 +1,57 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Organization
+    {
+
+
+        public List<Person> People { get; set; }
+        public String Name { get; set; }
+
+        public Organization(String databaseName)
+        {
+            Name = databaseName;
+            People = FakeService.GetPeople();
+        }
+
+
+
+        public void Add(Person person)      //add the person 
+        {
+            if (!People.Contains(person))
+            {
+                People.Add(person);
+                FakeService.Write(person);
+            }
+        }
+
+
+        public void Delete(Person person)       //delete the person
+        {
+            if (People.Contains(person))
+            {
+                People.Remove(person);
+                FakeService.Delete(person);
+            }
+        }
+
+
+
+        public void Update(Person person)       //update the person 
+        {
+            FakeService.Write(person);
+        }
+
+
+
+
+
+
+    }
+}
