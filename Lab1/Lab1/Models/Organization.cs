2c58d5aae8d4c42f0658d36f14b2504408933a72
sarab singh <sarab singh>
2016-09-28 18:18:59 +0100

    ModelViewAndViewModelApp

---
+++ Lab1/Lab1/Models/Organization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Models
+{
+    class Organization
+    {
+        public List<Person> People { get; set; }
+        public String Name { get; set; }
+
+        public Organization(String databaseName)
+        {
+            Name = databaseName;
+            People = FakeService.GetPeople();
+        }
+
+        public void Add(Person person)              //Add the person
+        {
+            if (!People.Contains(person))
+            {
+                People.Add(person);
+                FakeService.Write(person);
+            }
+        }
+
+        public void Delete(Person person)           //delete the person
+        {
+            if (People.Contains(person))
+            {
+                People.Remove(person);
+                FakeService.Delete(person);
+            }
+        }
+
+        public void Update(Person person)       //update the person
+        {
+            FakeService.Write(person);
+        }
+
+    }
+}
